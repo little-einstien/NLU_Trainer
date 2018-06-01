b59a9abde54c8be9c83863e71f6f82905ebce4cb
avsimr <arnavsimer@ymail.com>
2018-06-02 04:52:55 +0530

    update

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -89,8 +89,8 @@ export class DataHandlerService {
           'Content-Type': 'application/json'
         })
       };
-      //let url = 'http://localhost:4203/train/' + projid;
-      let url = 'http://35.200.131.47:4203/train/' + projid;
+      //let url = 'http://localhost:4205/train/' + projid;
+      let url = 'http://35.200.131.47:4205/train/' + projid;
       
       try {
         this.http.get(url).subscribe(res => {
