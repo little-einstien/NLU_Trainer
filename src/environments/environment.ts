0e00d4fcf389e3815119c95a0fb6857f08dd10a0
Little Einstien <arnavsimer@ymail.com>
2018-06-24 13:58:32 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   token :'ab091c04f7254475b9fd000e7bf96a0d',
-  // nlu_endpoint : 'http://35.200.142.0/4203/api',
-  // api_endpoint  : 'http://35.200.142.0:3000',
-  // bot_endpoint : 'http://35.200.142.0:3000',
-  nlu_endpoint : 'http://localhost:4203',
-  api_endpoint  : 'http://localhost:3000',
-  bot_endpoint : 'http://localhost:3000',
+   nlu_endpoint : 'http://35.200.142.0/4203/api',
+   api_endpoint  : 'http://35.200.142.0:3000',
+   bot_endpoint : 'http://35.200.142.0:3000',
+//   nlu_endpoint : 'http://localhost:4203',
+//   api_endpoint  : 'http://localhost:3000',
+//   bot_endpoint : 'http://localhost:3000',
   
   bot_script_pf : '<script type="text/javascript" id = "bot-script" data-project = ',
   bot_script_sf : '/plugin/js/chatBotWidget.js"></script>'
